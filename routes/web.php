c1c07f0da7b6b779151d73fa2d759114118535b6
anucha1993 <108377127+anucha1993@users.noreply.github.com>
2025-08-11 08:32:51 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -2,35 +2,34 @@
 
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+use Spatie\ResponseCache\Middlewares\CacheResponse;
 
 // Route::get('/', function () {
 //     return view('home');
 // })->name('home');
 
-Route::get('/',[HomeController::class, 'index'])->name('home');
 
-Route::get('/packages', function () {
-    return view('packages');
-})->name('packages');
-
-Route::get('/welcome', function () {
-    return view('welcome');
+// Apply response cache middleware to public routes
+Route::middleware([CacheResponse::class])->group(function () {
+    Route::get('/',[HomeController::class, 'index'])->name('home');
+    Route::get('/packages', function () {
+        return view('packages');
+    })->name('packages');
+    Route::get('/welcome', function () {
+        return view('welcome');
+    });
+    Route::get('/about', function () {
+        return view('about');
+    })->name('about');
+    Route::get('/contact', function () {
+        return view('contact');
+    })->name('contact');
 });
 
-// Authentication Routes
+// Authentication Routes (do not cache)
 Route::get('/login', function () {
     return view('auth.login');
 })->name('login');
-
 Route::get('/register', function () {
     return view('auth.register');
 })->name('register');
-
-// Additional Routes
-Route::get('/about', function () {
-    return view('about');
-})->name('about');
-
-Route::get('/contact', function () {
-    return view('contact');
-})->name('contact');
